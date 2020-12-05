8a0578b9cb63f652e223de7939f2eaf674769045
Victoria <vikiber2013@gmail.com>
2020-12-05 12:33:21 +0200

    heroku prepear

--- tes_project/settings.py
+++ tes_project/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # redirect to login if not logged in
 LOGIN_URL = reverse_lazy('login')
