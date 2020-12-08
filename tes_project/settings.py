01888ca6c13c338580998b20c0f2924feae8eff1
Victoria <vikiber2013@gmail.com>
2020-12-08 15:55:20 +0200

    lokal

--- tes_project/settings.py
+++ tes_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'w(pq2kd^^)f9+6go2h+p1%+ii&s@7d)(vnb_$%^3a*^z%5d6*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['testjulishcherbina.herokuapp.com', "127.0.0.1"]
 
